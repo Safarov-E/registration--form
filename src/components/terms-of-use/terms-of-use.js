7380c9d3019c9826332da284433df6ea6484c948
safarov-e <elkhan.safarov10@yandex.com>
2021-01-31 19:53:47 +0500

    Открыти модального окна при клике

--- src/components/terms-of-use/terms-of-use.js
+++ src/components/terms-of-use/terms-of-use.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import './terms-of-use.css'
+import {onModalDescriptionClose} from '../../store/actions/actions'
 
 class TermsOfUse extends Component {
     render() {
@@ -13,20 +14,15 @@ class TermsOfUse extends Component {
                     <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.  </p>
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
                     <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
-                    <button>Продолжить</button>
+                    <button onClick={() => this.props.onModalDescription()}>Продолжить</button>
                 </div>
             </div>
         )
     }
 }
-function mapStateToProps(state) {
-    return {
-        
-    }
-}
 function mapDispatchToProps(dispatch) {
     return {
-        
+        onModalDescription: () => dispatch(onModalDescriptionClose())
     }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(TermsOfUse);
+export default connect(null, mapDispatchToProps)(TermsOfUse);
